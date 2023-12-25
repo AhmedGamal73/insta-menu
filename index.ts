788f0baf0f97fc2102f3ddba6d82d0a7ad44f8c2
AhmedGamal73 <malokokha2@gmail.com>
2023-12-25 05:20:01 +0200

    add typescript typres in index.ts file

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import http from 'http';
+import http, { Server } from 'http';
 import app from './app';
 
-const server = http.createServer(app);
+const server: Server = http.createServer(app);
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT; // default port to listen
+const API_PORT: string | undefined = process.env.API_PORT;
+const port: string | number = process.env.PORT || API_PORT || 3000; // default port to listen
 
 server.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
